6ba85be1e0391721212a18166b31af98c572ca23
Tibor <tibor.molnar@waltoninstitute.ie>
2023-11-17 16:25:56 +0000

    frontend navbar created

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,4 +1,38 @@
+<script>
+    import {page} from "$app/stores"
+    let routeId;
+    $: routeId=$page.route.id;
+
+    const nav = [
+        { 
+            title: "Home",
+            href: "/"
+        },
+        {
+            title: "Services",
+            href: "/services"
+        },
+        {
+            title: "About",
+            href: "/about"
+        },
+        {
+            title: "Contact",
+            href: "/contact"
+        }
+    ];
+</script>
+
 <div class='main-app'>
+    <nav>
+        <ul>
+            {#each nav as {title, href }}
+            <li>
+                <a {href} class:active={ routeId === href} {title}>{title}</a>
+            </li>
+            {/each}
+        </ul>
+    </nav>
   <slot/>
   </div>
   
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,40 +1,7 @@
-<style>
-    a{
-        text-decoration: none;
-        color: #879ed9;
-    }
-    a:hover{
-        text-decoration: underline;
-    }
-    ul {
-        display: flex;
-        list-style: none;
-    }
-    li {
-        margin-right: 20px;
-    }
-</style>
-
 <script>
   import Title from "../pages/Title/+page.svelte";
 </script>
 
-<nav>
-    <ul>
-        <li>
-            <a href="/">Home</a>
-        </li>
-        <li>
-            <a href="/about">About</a>
-        </li>
-        <li>
-            <a href="/services">Services</a>
-        </li>
-        <li>
-            <a href="/contact">Contact</a>
-        </li>
-    </ul>
-</nav>
 
 <h1>Welcome to Test frontend and backend</h1>
 
