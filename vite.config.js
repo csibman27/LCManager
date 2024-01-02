ef12deb538c3db032972f559ba65ec644f0fe40c
Tibor <tibor.molnar@waltoninstitute.ie>
2024-01-02 14:56:00 +0000

    tidy test code 1

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,6 @@
 import { sveltekit } from '@sveltejs/kit/vite';
-import { preprocess } from 'svelte/compiler';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [
-		sveltekit(
-)]
+	plugins: [sveltekit()]
 });
